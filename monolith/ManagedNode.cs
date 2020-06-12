324895a7cc4a56d651a5de54840b64f1920d23a6
catSIXe <github2.konrad@sch.umann.it>
2020-06-13 00:50:38 +0200

    Auto stash before rebase of "master"

--- monolith/ManagedNode.cs
+++ monolith/ManagedNode.cs
@@ -1,5 +1,6 @@
 using Grpc.Net.Client;
-
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -9,35 +10,47 @@ namespace monolith
     internal class ManagedNode
     {
         private Program.Options o;
-
+        private ServiceCollection services { get; }
+        private ILoggerFactory logger { get; }
         public ManagedNode(Program.Options o)
         {
             this.o = o;
+            this.services = new ServiceCollection();
+            this.logger = LoggerFactory.Create(logging =>
+            {
+                logging.AddConsole();
+                logging.SetMinimumLevel(LogLevel.None);
+            });
         }
+
         public async Task Run()
         {
+            Uri Address = new Uri($"https://{ o.Host }:${ o.Port }");
+            services.AddGrpcClient<NodeRegistry.NodeRegistryClient>(o =>
+            {
+                o.Address = Address;
+            });
+            services.AddGrpcClient<FileRegistry.FileRegistryClient>(o =>
+            {
+                o.Address = Address;
+            });
+            ServiceProvider serviceProvider = services.BuildServiceProvider();
+            var provider = serviceProvider;
             // The port number(5001) must match the port of the gRPC server.
             try
             {
-                Console.WriteLine($"https://{ o.Host }:${ o.Port }");
-                var channelOptions = new GrpcChannelOptions
-                {
-                    // preparing for auth-stuff
-                    // Credentials = Grpc.Core.ChannelCredentials.Insecure, // Grpc.Core.ChannelCredentials.Create(Grpc.Core.ChannelCredentials.Insecure, Grpc.Core.CallCredentials.FromInterceptor())
-
-                    // HttpHandler = new ApiKeyHandler("MY_KEY"),
-                };
-                using var channel = GrpcChannel.ForAddress($"https://{ o.Host }:{ o.Port }", channelOptions);
+                Console.WriteLine(Address);
+                /*using var channel = GrpcChannel.ForAddress($"https://{ o.Host }:{ o.Port }");
 
                 var nodeRegistryClient = new NodeRegistry.NodeRegistryClient(channel);
-                var fileRegistryClient = new FileRegistry.FileRegistryClient(channel);
+                var fileRegistryClient = new FileRegistry.FileRegistryClient(channel);*/
                 { // Auth
-                    var reply = await nodeRegistryClient.AuthenticateAsync(
+                    var reply = await provider.GetRequiredService<NodeRegistry.NodeRegistryClient>().AuthenticateAsync(
                                 new NodeAuthenticationRequest { Identifier = o.ClientID });
                     Console.WriteLine($"Authentication as { o.ClientID }: " + reply.Status);
                 }
                 {
-                    var reply = await nodeRegistryClient.TestAsync(new NodeTestRequest { Identifier = o.ClientID });
+                    var reply = await provider.GetRequiredService<NodeRegistry.NodeRegistryClient>().TestAsync(new NodeTestRequest { Identifier = o.ClientID });
                     Console.WriteLine($"Authentication Test as { o.ClientID }: " + reply.Status);
                     if (reply.Status == false) {
                         Console.WriteLine("Something fucked up wrongly");
@@ -59,14 +72,14 @@ namespace monolith
                                 if (args.Length == 2)
                                 {
                                     var fileName = args[1];
-                                    var reply = await fileRegistryClient.RegisterAsync(
+                                    var reply = await provider.GetRequiredService<FileRegistry.FileRegistryClient>().RegisterAsync(
                                         new FileRegisterRequest { Filename = fileName });
                                     Console.WriteLine($"{ reply.Id }");
                                 }
                                 break;
                             case "browse":
                                 {
-                                    var reply = await fileRegistryClient.BrowseAsync(
+                                    var reply = await provider.GetRequiredService<FileRegistry.FileRegistryClient>().BrowseAsync(
                                         new FileBrowseRequest { });
                                     foreach (var file in reply.Files)
                                         Console.WriteLine(file);
@@ -76,7 +89,7 @@ namespace monolith
                                 if (args.Length == 2)
                                 {
                                     var id = args[1];
-                                    var reply = await fileRegistryClient.GetAsync(
+                                    var reply = await provider.GetRequiredService<FileRegistry.FileRegistryClient>().GetAsync(
                                         new FileGetRequest { Id = id });
                                     foreach (var fileName in reply.Where2Get)
                                         Console.WriteLine($"{ fileName }");
